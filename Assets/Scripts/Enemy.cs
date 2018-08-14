842fe5c7dad82afdb714fe589334f10c64976d61
Timothy Harrington <tharrington916@gmail.com>
2018-08-14 15:50:25 -0400

    added enemy, destruction effects

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] private GameObject deathFX;
+    [SerializeField] private Transform parent;
+
+
+    // Use this for initialization
+    void Start ()
+	{
+	    AddNonTriggerBoxCollider();
+	}
+
+    private void AddNonTriggerBoxCollider()
+    {
+        DestroyExistingBoxColliders();
+        Collider enemyCollider = gameObject.AddComponent<BoxCollider>();
+        enemyCollider.isTrigger = false;
+    }
+
+    private void DestroyExistingBoxColliders()
+    {
+        if (gameObject.GetComponents<BoxCollider>().Length > 0)
+        {
+            foreach (var boxCollider in gameObject.GetComponents<BoxCollider>())
+            {
+                Destroy(boxCollider);
+            }
+        }
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
