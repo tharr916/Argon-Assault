6a38f42095b1073853f854f6ccc977fffe1ec3e4
Timothy Harrington <tharrington916@gmail.com>
2018-08-14 15:50:49 -0400

    create singleton

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -3,7 +3,17 @@
 public class MusicPlayer : MonoBehaviour {
 	void Awake()
 	{
-		DontDestroyOnLoad(this.gameObject);        
+	    int numMusicPlayers =  FindObjectsOfType<MusicPlayer>().Length;
+
+	    if (numMusicPlayers > 1)
+	    {
+            Destroy(gameObject);
+	    }
+	    else
+	    {
+	        DontDestroyOnLoad(this.gameObject);
+        }
+		
 	}
 
 }
