1f7cd972c03ab3e0b47149612917c9894d616c24
Timothy Harrington <tharrington916@gmail.com>
2018-08-14 10:17:54 -0400

    move collision related events and handling to new class

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour {
+
+    #region lifecycle methods
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    #endregion
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        print("You triggered me.   You fucking TRIGGERED me.");
+    }
+}
