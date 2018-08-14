4a266ce0c351647fc6bb5d2cbce7cf48d742911b
Timothy Harrington <tharrington916@gmail.com>
2018-08-14 09:17:43 -0400

    organization and lighting change - will need to regen lighting at times

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,25 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour {
 	void Awake()
 	{
 		DontDestroyOnLoad(this.gameObject);        
 	}
-	// Use this for initialization
-	void Start () {
-		Invoke("LoadFirstScene", 2f);
-	}
 
-	void LoadFirstScene()
-	{
-		SceneManager.LoadScene(1);
-	}
-
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("LoadFirstScene", 2f);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
