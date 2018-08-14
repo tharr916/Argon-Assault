3aa77738f8c4ff757294c950af14e34fe505ec09
Timothy Harrington <tharrington916@gmail.com>
2018-08-14 15:51:28 -0400

    enemy explosion effects will destroy after a set time

---
+++ Assets/Scripts/SelfDestructor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructor : MonoBehaviour
+{
+    [SerializeField] private float timeTillDestroyed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, timeTillDestroyed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
