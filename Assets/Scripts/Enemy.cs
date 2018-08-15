30c889c47854371b37cec18a8f2bc721fbea9ce8
Timothy Harrington <tharrington916@gmail.com>
2018-08-15 08:17:33 -0400

    add hitpoints

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,21 +8,35 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] private GameObject deathFX;
     [SerializeField] private Transform parent;
-    [SerializeField]
-    int scorePerHit = 12; // TODO should this be associated with the enemy instead?
+    [SerializeField] int scorePerHit = 12;
+    [SerializeField] private int hits = 10;
 
     private ScoreBoard _scoreBoard;
 
     // Use this for initialization
     void Start ()
-	{
-	    AddNonTriggerBoxCollider();
-	    _scoreBoard = FindObjectOfType<ScoreBoard>();
-	}
+    {
+        AddNonTriggerBoxCollider();
+        _scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
 
     private void OnParticleCollision(GameObject other)
     {
+        ProcessHit();
+        if (hits <= 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void ProcessHit()
+    {
         _scoreBoard.ScoreHit(scorePerHit);
+        hits--;
+    }
+
+    private void KillEnemy()
+    {
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
         Destroy(gameObject);
